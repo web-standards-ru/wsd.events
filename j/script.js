37a50d0f67ed7b4673aee3ceb8b66180348a02d8
Mikhail Baranov <mikhail.baranov@gmail.com>
2011-10-29 17:44:42 +0400

    Изменил функцию map.
    Сначала проверяем, возвращает ли document.getElementById(id), ссылку на элемент, а потом уже делаем всё.

--- j/script.js
+++ j/script.js
@@ -33,7 +33,7 @@ $(function(){
 			return string.replace( /^\//, '' ).replace( /\/$/, '' );
 		}
 	}
-	
+
 	$('a[href^=#]').scroller();
 
 	// Tweets
@@ -49,11 +49,11 @@ $(function(){
 				text = text.replace(
 					/(^|\s)(?:#([\d\w_]+)|@([\d\w_]{1,15}))|(https?:\/\/[^\s"]+[\d\w_\-\/])|([^\s:@"]+@[^\s:@"]*)/gi,
 					function( all, space, hashtag, username, link, email ) {
-					    var res = '<a href="mailto:' + email + '">' + email + "</a>";
-						    hashtag && (res = space + '<a href="http://search.twitter.com/search?q=%23' + hashtag + '">#' + hashtag + "</a>");
-						    username && (res = space + '<a href="http://twitter.com/' + username + '">@' + username + "</a>");
-						    link && (res = '<a href="' + encodeURI(decodeURI(link.replace(/<[^>]*>/g, ""))) + '">' + link + "</a>");
-					    return res;
+						var res = '<a href="mailto:' + email + '">' + email + "</a>";
+							hashtag && (res = space + '<a href="http://search.twitter.com/search?q=%23' + hashtag + '">#' + hashtag + "</a>");
+							username && (res = space + '<a href="http://twitter.com/' + username + '">@' + username + "</a>");
+							link && (res = '<a href="' + encodeURI(decodeURI(link.replace(/<[^>]*>/g, ""))) + '">' + link + "</a>");
+						return res;
 					}
 				);
 				list.append(
@@ -73,53 +73,55 @@ $(function(){
 
 	$.fn.map = function() {
 		var map = document.getElementById( this.selector.substr( 1 ) ),
-			query = map.firstChild.src.split('&'),
+			query,
 			centerCoords = {}, markerCoords = {}, coords, zoomLevel;
-		for(var i=0, queryLength = query.length; i < queryLength; i++ ) {
-			var current = query[i];
-			if( current.indexOf('center')+1 ) {
-				coords = current.split('=')[1].split(',');
-				centerCoords.lat = coords[0];
-				centerCoords.lng = coords[1];
+		if (map != null) {
+			for(var i=0, queryLength = query.length; i < queryLength; i++ ) {
+				var current = query[i];
+				if( current.indexOf('center')+1 ) {
+					coords = current.split('=')[1].split(',');
+					centerCoords.lat = coords[0];
+					centerCoords.lng = coords[1];
+				}
+				if( current.indexOf('markers')+1 ) {
+					coords = current.split('=')[1].split(',');
+					markerCoords.lat = coords[0];
+					markerCoords.lng = coords[1];
+				}
+				if( current.indexOf('zoom')+1 ) {
+					zoomLevel = parseInt(current.split('=')[1]);
+				}
 			}
-			if( current.indexOf('markers')+1 ) {
-				coords = current.split('=')[1].split(',');
-				markerCoords.lat = coords[0];
-				markerCoords.lng = coords[1];
+			var options = {
+				zoom: zoomLevel,
+				center: new google.maps.LatLng( centerCoords.lat, centerCoords.lng ),
+				mapTypeControl: false,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
 			}
-			if( current.indexOf('zoom')+1 ) {
-				zoomLevel = parseInt(current.split('=')[1]);
-			}
+			var gmap = new google.maps.Map(
+				map, options
+			);
+			var icon = new google.maps.MarkerImage(
+				'/i/map-logo.png',
+				new google.maps.Size( 93, 83 ),
+				new google.maps.Point( 0, 0 ),
+				new google.maps.Point( 24, 83 )
+			);
+			var shadow = new google.maps.MarkerImage(
+				'/i/map-shadow.png',
+				new google.maps.Size( 93, 83 ),
+				new google.maps.Point( 0, 0 ),
+				new google.maps.Point( 24, 83 )
+			);
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng( markerCoords.lat, markerCoords.lng ),
+				map: gmap,
+				shadow: shadow,
+				icon: icon
+			});
 		}
-		var options = {
-			zoom: zoomLevel,
-			center: new google.maps.LatLng( centerCoords.lat, centerCoords.lng ),
-			mapTypeControl: false,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-		var gmap = new google.maps.Map(
-			map, options
-		);
-		var icon = new google.maps.MarkerImage(
-			'/i/map-logo.png',
-			new google.maps.Size( 93, 83 ),
-			new google.maps.Point( 0, 0 ),
-			new google.maps.Point( 24, 83 )
-		);
-		var shadow = new google.maps.MarkerImage(
-			'/i/map-shadow.png',
-			new google.maps.Size( 93, 83 ),
-			new google.maps.Point( 0, 0 ),
-			new google.maps.Point( 24, 83 )
-		);
-		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng( markerCoords.lat, markerCoords.lng ),
-			map: gmap,
-			shadow: shadow,
-			icon: icon
-		});
 	}
-	
+
 	$('#map').map();
 
 	window.onerror = function(){
