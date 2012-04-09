4c37fe16176b87c6e525277e526e23e88db2d074
Vadim Makeev <pepelsbey@gmail.com>
2012-04-09 15:01:46 +0400

    Презентации и браузерные войны удалены из Git

--- 2011/10/29/pres/editable/examples/js/ie-refresh.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function () {
-  var wrapper = $(".site")[0];
-  wrapper.style.zoom = "2";
-  wrapper.style.zoom = "1";
-  
-});
--- 2011/12/13/pres/css/j/script.ie.js
+++
@@ -1,3 +0,0 @@
-/*_*/
-
-/* HTML5 doctor */ (function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,canvas,datalist,details,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
