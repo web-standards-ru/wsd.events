feaed0666f2d558eb75bd65957ed8ef6eba02a37
Mikhail Baranov <mikhail.baranov@gmail.com>
2012-10-23 02:31:06 +0400

    Получаем доклад по дате

--- app.py
+++ app.py
@@ -2,9 +2,13 @@
 
 import json
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+def add_null(val):
+    return val if val >= 10 else "0%s" % val
+
 
 @app.route('/')
 def hello_world():
@@ -14,8 +18,20 @@ def hello_world():
 
 @app.route('/<int:year>/<int:month>/<int:day>/')
 def event(year, month, day):
+    data = {}
+    for i in ['events', 'partners', 'presentations', 'speakers']:
+        data[i] = json.load(open('data/%s.json' % i))
 
-    return u'Событие'
+    event_date = '%s-%s-%s' % (add_null(day), add_null(month), year)
+
+    try:
+        event = filter(lambda x: x if x['date'] == event_date else False, data['events'].values())[0]
+    except IndexError:
+        return render_template('page_not_found.html'), 404
+    except Exception:
+        return render_template('server_error.html'), 500
+
+    return unicode(event)
 
 
 @app.route('/<int:year>/<int:month>/<int:day>/register/')
