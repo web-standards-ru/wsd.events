0a2b7ade4c1a13410e7810b35159f0a1888eb6f3
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-07-18 21:13:27 +0400

    Расширил файл конфигурации

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 # coding=utf-8
+import os
 from utils.system import get_env_variable
 
-CSRF_ENABLED = True
-SECRET_KEY = get_env_variable('WSD_SECRET_KEY')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
+
+
+class Config(object):
+    CSRF_ENABLED = True
+    SECRET_KEY = get_env_variable('WSD_SECRET_KEY')
--- wsd/__init__.py
+++ wsd/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from flask import Flask
 
 app = Flask(__name__, static_folder='../templates/static', template_folder='../templates')
-app.config.from_object('config')
+app.config.from_object('config.Config')
 
 from .views import *
