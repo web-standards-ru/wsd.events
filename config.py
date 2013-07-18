fbd4f6d915d594c38671412c6c2edc81ca583574
Mikhail Baranov <mikhail.baranov@gmail.com>
2013-07-19 01:46:38 +0400

    Вынес пути к статике и шаблонам в конфиг

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
 PRESENTATIONS_DIR = os.path.join(PROJECT_ROOT, 'pres')
 
+TEMPLATES_DIR = os.path.join(PROJECT_ROOT, 'templates')
+STATIC_DIR = os.path.join(TEMPLATES_DIR, 'static')
+
 
 class Config(object):
     CSRF_ENABLED = True
