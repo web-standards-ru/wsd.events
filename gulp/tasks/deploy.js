1a90408e16f983a69028e4a72781983e92643e31
Vadim Makeev <pepelsbey@gmail.com>
2015-11-09 02:50:47 +0100

    Уточнил деплой

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -8,9 +8,11 @@ var gulp = require('gulp'),
 gulp.task('deploy', function() {
 	return gulp.src('dest/**')
 		.pipe(rsync({
+			root: 'dest',
 			hostname: 'web-standards.ru',
 			destination: '/var/www/wsd.events/www/htdocs/',
 			recursive: true,
+			clean: true,
 			incremental: true,
 			times: true
 		}));
