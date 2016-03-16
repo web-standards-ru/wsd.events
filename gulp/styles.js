40bfca4e775b190b10eeabf1ac1adfe8b01fe857
Vadim Makeev <pepelsbey@gmail.com>
2016-03-16 13:38:58 +0300

    Нормально инлайним SVG

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,16 +1,21 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const csso = require('gulp-csso');
+const autoprefixer = require('autoprefixer');
 const concat = require('gulp-concat');
+const csso = require('gulp-csso');
+const gulp = require('gulp');
+const svg = require('postcss-inline-svg');
 const merge = require('merge-stream');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
 const sync = require('browser-sync').get('sync');
 
 gulp.task('styles', () => {
 	// Building main styles into screen.css
 	const screen = gulp.src('src/styles/screen.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer())
+		.pipe(postcss([
+			autoprefixer,
+			svg
+		]))
 		.pipe(csso())
 		.pipe(gulp.dest('dest/styles'))
 		.pipe(sync.stream());
