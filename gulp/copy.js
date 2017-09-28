1f75a1dcdb32909e370c6a2e9f6507a95d3cef8d
Vadim Makeev <pepelsbey@gmail.com>
2017-09-29 01:51:37 +0300

    Перемещает спикеров в специальное копирование

--- gulp/copy.js
+++ gulp/copy.js
@@ -2,9 +2,7 @@ const gulp = require('gulp');
 const sync = require('browser-sync').get('sync');
 
 gulp.task('copy', () => {
-	return gulp.src('src/assets/**', {
-			dot: true
-		})
+	return gulp.src('src/assets/**')
 		.pipe(gulp.dest('dest'))
 		.pipe(sync.stream({
 			once: true
