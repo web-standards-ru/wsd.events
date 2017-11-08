9bf432b2ae1bf49ff1ff374b1a4878866098d1a5
Vadim Makeev <pepelsbey@gmail.com>
2017-11-09 02:57:03 +0300

    Собирает теперь все страницы, а не только события

--- gulp/html.js
+++ gulp/html.js
@@ -4,15 +4,19 @@ const rename = require('gulp-rename');
 const sync = require('browser-sync').get('sync');
 
 gulp.task('html', () => {
-	return gulp.src('src/pages/**/*.html')
+	return gulp.src('src/pages/*.html')
 		.pipe(htmlmin({
 			removeComments: true,
 			collapseWhitespace: true
 		}))
 		.pipe(rename((path) => {
-			const regex = /(\d{4})-(\d{2})-(\d{2})-\w+/;
-			if (regex.test(path.basename)) {
-				path.dirname = path.basename.replace(regex, '$1/$2/$3');
+			if (path.basename != 'index') {
+				const events = /(\d{4})-(\d{2})-(\d{2})-\w+/;
+				if (events.test(path.basename)) {
+					path.dirname = path.basename.replace(events, '$1/$2/$3');
+				} else {
+					path.dirname = path.basename;
+				}
 				path.basename = 'index';
 			}
 		}))
