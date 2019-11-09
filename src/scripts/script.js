bbbe0d9431cf1c72c87a2c4af17483410e9a8fde
Денис Балыко <denisbalyko@gmail.com>
2019-11-09 12:24:45 +0300

    Добавляет гард для загрузки карты (#186)

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -37,15 +37,27 @@
 
 // Maps
 
-function getMapData() {
+function getMapData(allowedHostname) {
 	var map = document.getElementById('map');
-	var src = map.firstChild.src.split('?')[1].split('&');
+
+	if (!allowedHostname || !map) return;
+
+	var rawSrc = map.firstChild.src;
+
+	if (typeof rawSrc !== 'string') return;
+
+	var srcPrefix = rawSrc.split('?')[0];
+	var srcSuffix = rawSrc.split('?')[1];
+	var srcParams = srcSuffix.split('&');
+
+	if (srcPrefix.indexOf(allowedHostname) === -1) return;
+
 	var data = {
 		map: map
 	};
 
-	for (var i = 0; i < src.length; i++) {
-		var item = src[i].split('=');
+	for (var i = 0; i < srcParams.length; i++) {
+		var item = srcParams[i].split('=');
 		data[item[0]] = item[1];
 	}
 
@@ -53,7 +65,9 @@ function getMapData() {
 }
 
 function yandexMap() {
-	var data = getMapData();
+	var data = getMapData('maps.yandex.ru');
+
+	if (!data) return;
 
 	data.ll = data.ll.split(',');
 	data.latitude = parseFloat(data.ll[0]);
@@ -84,7 +98,9 @@ function yandexMap() {
 }
 
 function googleMap() {
-	var data = getMapData();
+	var data = getMapData('maps.googleapis.com');
+
+	if (!data) return;
 
 	data.center = data.center.split(',');
 	data.latitude = parseFloat(data.center[0]);
@@ -102,7 +118,7 @@ function googleMap() {
 		center: coordinates
 	});
 
-	var marker = new google.maps.Marker({
+	new google.maps.Marker({
 		position: coordinates,
 		icon: {
 			url: '/images/icon.svg',
