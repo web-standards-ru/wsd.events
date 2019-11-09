c31b7a00b50a110e5183c05d0bea74d88062de80
Mikhail Baranov <mikhail.baranov@gmail.com>
2019-11-09 12:41:26 +0300

    Fixes permission issues on deployment

--- gulpfile.js
+++ gulpfile.js
@@ -153,6 +153,7 @@ gulp.task('deploy', () => {
 	return gulp.src('dest/**')
 		.pipe(rsync({
 			root: 'dest',
+			username: 'travis',
 			hostname: 'web-standards.ru',
 			destination: '/var/www/wsd.events/html/',
 			recursive: true,
